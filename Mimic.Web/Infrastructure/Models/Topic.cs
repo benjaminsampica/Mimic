211ce7a931bec3494ea16ac2b6ed4541d208832f
Ben Sampica <benjamin.sampica@gmail.com>
2022-08-03 20:50:53 -0500

    Update Topic.cs

--- Mimic.Web/Infrastructure/Models/Topic.cs
+++ Mimic.Web/Infrastructure/Models/Topic.cs
@@ -3,7 +3,7 @@
 public class Topic : IEntity
 {
     public string Id { get; set; } = nameof(Topic) + Guid.NewGuid();
-    public string Name { get; set; } = null!;
+    public string Summary { get; set; } = null!;
     public string Body { get; set; } = null!;
     public string[] Tags { get; set; } = null!;
 }
